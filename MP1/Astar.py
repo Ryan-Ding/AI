8c08c792d564a4cf95745479eedc681efdb005b8
Tianyu Sun <halcyon135@yahoo.com>
2017-09-24 23:40:29 -0500

    modify solutions to show # of nodes expanded and # of steps in the solution

--- MP1/Astar.py
+++ MP1/Astar.py
@@ -1,28 +1,28 @@
 from Graph import Graph
 
 def find_path():
-    while (len(open_set)):
+    while (len(frontier)):
         global current_pos
         current_pos = pos_lowest_f_score()
         if (current_pos == graph.goals[0]):
-            print_path()
+            print_solution()
             return
 
-        open_set.remove(current_pos)
-        closed_set.add(current_pos)
+        frontier.remove(current_pos)
+        graph.mark_visited(current_pos)
         current_neighbors = graph.get_neighbors(current_pos)
 
         for neighbor in current_neighbors:
-            if neighbor in closed_set:
+            if graph.has_visited(neighbor):
                 continue       # Ignore the neighbor if it is already evaluated.
             tentative_g_score = g_score[current_pos] + 1
-            if neighbor not in open_set:
-                open_set.add(neighbor)      # Discovered a new node
+            if neighbor not in frontier:
+                frontier.add(neighbor)      # Discovered a new node
             elif tentative_g_score >= g_score[neighbor]:
                 continue       # This is not a better path.
 
             # This path is the best until now. Record it!
-            came_from[neighbor] = current_pos;
+            graph.came_from[neighbor] = current_pos;
             g_score[neighbor] = tentative_g_score;
             f_score[neighbor] = g_score[neighbor] + heuristic_estimate(neighbor, graph.goals)
 
@@ -32,7 +32,7 @@ def find_path():
 def pos_lowest_f_score():
     lowest_f = float("inf")
     pos_lowest_f = ()
-    for pos in open_set:
+    for pos in frontier:
         f_score_from_this_position = f_score[pos]
         if f_score_from_this_position < lowest_f:
             lowest_f = f_score_from_this_position
@@ -44,15 +44,16 @@ def heuristic_estimate(pos, goals):
     vertical = abs(pos[1] - goals[0][1])
     return horizontal + vertical       # using manhattan distance
 
-def print_path():
+def print_solution():
     path_set = set()      # The set of all nodes on the solution path
     pos_on_path = current_pos      # Initially the goal position
 
-    while pos_on_path in came_from:     # Go back from goal to start position to retrive the nodes on path
+    while pos_on_path in graph.came_from:     # Go back from goal to start position to retrive the nodes on path
         path_set.add(pos_on_path)
-        pos_on_path = came_from[pos_on_path]
+        pos_on_path = graph.came_from[pos_on_path]
 
-    with open("mediumMaze_solution_astar.txt", "w") as fout:
+    with open("openMaze_solution_astar.txt", "w") as fout:
+        fout.write("%d nodes expanded | %d steps taken\n"%(len(graph.visited), len(path_set)))
         for i in range(len(graph.matrix)):
             for j in range(len(graph.matrix[i])):
                 pos = (i, j)
@@ -66,12 +67,10 @@ def print_path():
                     fout.write("%")
             fout.write("\n")
 
-graph = Graph("mediumMaze.txt")
+graph = Graph("openMaze.txt")
 current_pos = ()
 
-closed_set = set()     # The set of nodes already evaluated.
-open_set = set()       # The set of currently discovered nodes that are not evaluated yet.
-came_from = {}         # For each node, which node it can most efficiently be reached from.
+frontier = set()       # The set of currently discovered nodes that are not evaluated yet.
 
 g_score = {}        # For each node, the cost of getting from the start node to that node.
 f_score = {}        # Through each node, the total cost of getting from the start node to the goal.
@@ -81,7 +80,7 @@ for i in range(len(graph.matrix)):
         g_score[(i, j)] = float("inf")
         f_score[(i, j)] = float("inf")
 
-open_set.add(graph.start_position)
+frontier.add(graph.start_position)
 g_score[graph.start_position] = 0
 f_score[graph.start_position] = heuristic_estimate(graph.start_position, graph.goals)
 
