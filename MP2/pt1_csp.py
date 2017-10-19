15b061fe888d117320fc491e61ecb607078d612d
angelicali <liyunshuangelica@gmail.com>
2017-10-19 15:18:51 -0500

    [MP2-part1] dumb solver working

--- MP2/pt1_csp.py
+++ MP2/pt1_csp.py
@@ -1,4 +1,3 @@
-
 class CSP:
     def __init__(self, filename):
         self.variables = set()
@@ -6,17 +5,59 @@ class CSP:
         self.graph = None
         self.parse_graph(filename)
 
-    def parse_graph(self, filename):
-        pass
-
     def is_source(self, position):
-        pass
+        x, y = position
+        return self.graph[x][y] != '_'
 
-    def select_unassigned_variable(self, assignments):
-        pass
+    def get_color(self, position):
+        x, y = position
+        return self.graph[x][y]
 
     def get_neighbors(self, position):
-        pass
+        pos_lst = CSP.__get_surrounding_positions(position)
+        neighbors = []
+        for neighbor_position in pos_lst:
+            if self.is_on_graph(neighbor_position):
+                neighbors.append(neighbor_position)
+        return neighbors
 
     def is_complete(self, assignments):
         return len(assignments) == len(self.variables)
+
+    def print_solution(self, assignments):
+        height = len(self.graph)
+        width = len(self.graph[0])
+        for x in range(height):
+            for y in range(width):
+                position = (x,y)
+                if position in self.variables:
+                    color = assignments[position]
+                else:
+                    color = self.get_color(position)
+                print(color, end='')
+            print()
+
+    def is_on_graph(self, position):
+        x, y = position
+        height = len(self.graph)
+        width = len(self.graph[0])
+        return width > x >= 0 and 0 <= y < height
+
+    def parse_graph(self, filename):
+        with open(filename, 'r') as maze_file:
+            self.graph = maze_file.read().splitlines()
+
+        for i in range(len(self.graph)):
+            line = self.graph[i]
+            self.graph[i] = list(line)
+            for j in range(len(line)):
+                c = line[j]
+                if c == "_":
+                    self.variables.add((i, j))
+                else:
+                    self.all_colors.add(c)
+
+    def __get_surrounding_positions(position):
+        row = position[0]
+        col = position[1]
+        return [(row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col)]
--- MP2/pt1_dumb.py
+++ MP2/pt1_dumb.py
@@ -1,12 +1,53 @@
+from collections import Counter
+
 from pt1_csp import CSP
 
+def get_current_color(csp, assignments, position):
+    if csp.is_source(position):
+        return csp.get_color(position)
+    elif position in assignments:
+        return assignments[position]
+    else:
+        return None
+
 def can_assign(csp, var, val, assignments):
+    assignments[var] = val
+    print("checking can_assign({},{})".format(var, val))
+
+    if not is_valid(csp, assignments, var):
+        del assignments[var]
+        return False
+
     neighbors = csp.get_neighbors(var)
-    same_color_count = 0
     for neighbor in neighbors:
-        if neighbor in assignments and assignments[neighbor] == val:
-            same_color_count += 1
-    return same_color_count <= 2
+        if not is_valid(csp, assignments, neighbor):
+            del assignments[var]
+            return False
+    print("can assign({},{})!".format(var, val))
+    return True
+
+def is_valid(csp, assignments, position):
+    neighbors = csp.get_neighbors(position)
+    color = get_current_color(csp, assignments, position)
+    if not color:
+        return True
+
+    counter = Counter()
+    for neighbor in neighbors:
+        neighbor_color = get_current_color(csp, assignments, neighbor)
+        if neighbor_color:
+            counter[neighbor_color] += 1
+    num_colored = sum(counter.values())
+    num_empty = len(neighbors) - num_colored
+    same_color_count = counter[color]
+    if num_empty == 0:
+        if csp.is_source(position):
+            return same_color_count == 1
+        else:
+            return same_color_count == 2
+    else:
+        return same_color_count <= 2
+
 
 def domain_values(var, assignments, csp):
     return csp.all_colors
@@ -17,20 +58,30 @@ def select_unassigned_variable(csp, assignments):
 
 def assign(var, val, assignments, csp):
     assignments[var] = val
+    print("assign: {} = {}".format(var, val))
 
-def backtrack(assignments, remaining_values, csp):
+
+def backtrack(assignments,  csp):
+    # print("backtrack({})".format(assignments))
     if csp.is_complete(assignments):
         return assignments
     var = select_unassigned_variable(csp, assignments)
+    # print("selected {}".format(var))
     for val in domain_values(var, assignments, csp):
-        if can_assign(var, val, assignments, csp):
+        # print("checking {} ?= {}".format(var, val))
+        if can_assign(csp, var, val, assignments):
             assign(var, val, assignments, csp)
-            result = backtrack(assignments, remaining_values, csp)
-            if result:
+            result = backtrack(assignments,  csp)
+            if result != False:
                 return result
             else:
+                print("removing assignment for {}".format(var))
                 del assignments[var]
     return False
 
 
-csp = CSP('pt1_insput55_sample.txt')
+csp = CSP('pt1_55input_sample.txt')
+final_assignments = backtrack({}, csp)
+csp.print_solution(final_assignments)
+print(final_assignments)
+
